da8fb49f2b127f8a68579566c89000e4dc846e3c
UnSamyPro <40127757+UnSamyPro@users.noreply.github.com>
2020-03-14 01:39:44 +0100

    commit
    Co-authored-by: Unluckymichell

--- main/Core.js
+++ main/Core.js
@@ -1,11 +1,10 @@
 const events = require("events");
-const DeviceAPI = require("./deviceAPI");
+const deviceAPI = require("./deviceAPI");
 
 module.exports = class Core extends events.EventEmitter {
     constructor(config) {
         super();
         this.config = config;
- 
-        this.deviceAPI = new DeviceAPI();
+        this.deviceAPI = deviceAPI;
     }
 }
--- main/deviceAPI.js
+++ main/deviceAPI.js
@@ -1,17 +1,71 @@
-// Imports
-const express = require("express");
-const app = express();
+// Default
+EVENTNAMES = {
+  WHO_ARE_YOU: "whoareyou",
+  I_AM: "iam"
+};
 
-app.get('/', function (req, res) {});
+/*
 
-/**
- * @param {Number} port 
- * @param {Function} callback 
+{
+  "name": "Lampe",
+  "features": [
+    {
+      "type": "Actuator"
+      "name": "Lampe an.",
+      "eventname": "LampOn",
+      "args": []
+    },
+    {
+      "type": "get"
+      "name": "Get State",
+      "eventname": "getLampState",
+      "args": []
+    }
+  ]
+}
+
+
  */
-function listen(port, callback) {
-  app.listen();
-}
 
-module.exports = {
-  listen
-}
+
+// Imports
+const events = require("events");
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.clients
+
+wss.on('connection', function connection(ws) {
+
+  ws.device = {
+    id: null,
+    name: null,
+    features: []
+  }
+
+  ws.on('message', function incoming(messageRaw) {
+    
+    var message = null;
+    try {
+      message = JSON.parse(messageRaw);
+    } catch(err) {
+      return;
+    } finally {
+      if(typeof message.event == "undefined") return;
+      if(typeof message.data == "undefined") return;
+
+      switch(message.event) {
+        case EVENTNAMES.I_AM:
+          ws.name = data.name;
+          break;
+      }
+    }
+  });
+
+  ws.send(JSON.stringify({event: EVENTNAMES.WHO_ARE_YOU, data: null}));
+});
+
+
+
+module.exports;
