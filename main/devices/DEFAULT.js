32d4c742a23dbbe29413e99cd287920b27172b3a
Unluckymichell <39999330+Unluckymichell@users.noreply.github.com>
2020-03-15 16:49:35 +0100

    !Unfinished! Added device Manager.

--- main/devices/DEFAULT.js
+++ main/devices/DEFAULT.js
@@ -1,27 +1,22 @@
-/**
- *      Server          Client
- *     ------------------------
- *          <-- new  Connection
- *          --> SEND.GET_DEVICE_INFO
- *          <-- RECEIVE.DEVICE_INFO
- * 
- *          --> SEND.REJECT_CONNECTION
- *          --> SEND.ACCEPT_CONNECTION
- *          
- *          
- */
+
 
 module.exports = {
+    REASON: {
+        CLOSE_REQUESED: "sys_close_requested"
+    },
     EVENTNAME: {
         SEND: {
-            GET_DEVICE_INFO: "get_device_info",
-            REJECT_CONNECTION: "reject_conn",
-            ACCEPT_CONNECTION: "accept_conn"
-
+            GET_DEVICE_INFO: "sys_get_device_info",
+            REJECT_CONNECTION: "sys_reject_conn",
+            ACCEPT_CONNECTION: "sys_accept_conn",
+            CLOSE_CONNECTION: "sys_close_conn",
+            UPDATE_DEVICE_INFO: "sys_update_d_info"
         },
         RECEIVE: {
-            DEVICE_INFO: "device_info",
-            CLOSE_CONNECTION: "close_conn"
+            DEVICE_INFO: "sys_d_info",
+            CLOSE_CONNECTION: "sys_close_conn",
+            ACCEPT_DEVICE_INFO: "sys_accept_d_info",
+            REJECT_DEVICE_INFO: "sys_reject_d_info"
         }
     }
 }
